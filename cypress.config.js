0db40db1d1a90091e55e2ae9e174a0a167dfb507
Maria Sampaio <maria.sampaio@me.com.br>
2024-08-09 13:30:09 -0300

    add cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -2,13 +2,11 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
+  projectId: '',
   e2e: {
     baseUrl: 'https://notes-serverless-app.com',
     env: {
       viewportWidthBreakpoint: 768,
-    },
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    }
   },
 })
