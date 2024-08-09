6bf19902d7289e66381241679a57c23aa7d929fb
Maria Sampaio <maria.sampaio@me.com.br>
2024-08-09 13:38:48 -0300

    fix default command timeout and add project id

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,12 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
-  projectId: '',
+  projectId: 't7n1k8',
   e2e: {
     baseUrl: 'https://notes-serverless-app.com',
     env: {
       viewportWidthBreakpoint: 768,
     }
   },
+  'defaultCommandTimeout': 10000,
 })
